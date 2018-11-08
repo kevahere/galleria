b0c221461c28644c6f54ff3ffd8d463f635026c8
kevahere <kevahere@gmail.com>
2018-11-08 10:44:56 +0300

    added settings for finding static folder

--- galleria/settings.py
+++ galleria/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
+#STATICFILES_Storage = 'whitenoise.storage'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR,'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
