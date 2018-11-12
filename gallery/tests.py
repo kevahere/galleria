0ca59d31360c0cb6979751b94830940f5cf4f948
kevahere <kevahere@gmail.com>
2018-11-12 12:41:10 +0300

    deployment

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,8 +1,25 @@
+import datetime as dt
 from django.test import TestCase
-from .models import Location, Category, Images
+from .models import Editor,Image,tags
 
-# Create your tests here.
 
+
+#Tests for Images
 class ImageTestClass(TestCase):
-    #Setup method
-    def 
+    def setUp(self):
+
+        #Creating a new tag and saving it
+        self.new_tag = tags(name ='testing')
+        self.new_tag.save()
+
+        #Creating a new Image and saving it
+        self.new_Image=Image(name ='TestImage', image = 'images/TestImage.jpg', pub_date = dt.time(), location = 'Nairobi', category = 'Fun' )
+        self.new_Image.save()
+        self.new_Image.tags.add(self.new_tag)
+
+    def tearDown(self):
+        category.objects.all().delete()
+        tags.objects.all().delete()
+        Image.objects.all().delete()
+
+   
